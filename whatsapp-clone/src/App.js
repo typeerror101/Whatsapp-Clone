d74dd1c4808543e7d2e111609c4be1d0647a66de
Larosh Benny <57269077+typeerror101@users.noreply.github.com>
2022-12-16 00:10:34 +0530

    Initial commit

---
+++ whatsapp-clone/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Lets build a whatasap p clone</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ whatsapp-clone/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyChW4xWQnrRezSR9P0ZZ64kyAlboVvwD3E",
+  authDomain: "whatsapp-clone-42099.firebaseapp.com",
+  projectId: "whatsapp-clone-42099",
+  storageBucket: "whatsapp-clone-42099.appspot.com",
+  messagingSenderId: "800180114770",
+  appId: "1:800180114770:web:c7998ec1acc8de58b4968b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
